9f8ad55c85c798bf4e76abb538037f5cf2ec781f
nKadykov <nkadykov@gmail.com>
2024-03-18 20:25:33 +0300

    init

--- calculator.cpp
+++ calculator.cpp
@@ -21,10 +21,32 @@ Calculator::Calculator(QWidget* parent) : QWidget(parent), sumInMemory(0.0), sum
     Button *clearButton = createButton(tr("Clear"), &Calculator::clear);
     Button *clearAllButton = createButton(tr("Clear All"), &Calculator::clearAll);
     Button *clearMemoryButton = createButton(tr("MC"), &Calculator::clearMemory);
+    Button *readMemoryButton = createButton(tr("MR"), &Calculator::readMemory);
     Button *setMemoryButton = createButton(tr("MS"), &Calculator::setMemory);
-    Button *addtoMemoryButton = createButton(tr("M+"), &Calculator::addToMemory);
+    Button *addToMemoryButton = createButton(tr("M+"), &Calculator::addToMemory);
     Button *divisionButton = createButton(tr("\303\267"), &Calculator::multiplicativeOperatorClicked);
     Button *timesButton = createButton(tr("\303\227"), &Calculator::multiplicativeOperatorClicked);
     Button *minusButton = createButton(tr("-"), &Calculator::additiveOperatorClicked);
     Button *plusButton = createButton(tr("+"), &Calculator::additiveOperatorClicked);
+    Button *squareRootButton = createButton(tr("Sqrt"), &Calculator::unaryOperatorClicked);
+    Button *powerButton = createButton(tr("x\302\262"), &Calculator::unaryOperatorClicked);
+    Button *reciprocalButton = createButton(tr("1/x"), &Calculator::unaryOperatorClicked);
+    Button *equalButton = createButton(tr("="), &Calculator::equalClicked);
+
+    QGridLayout *mainLayout = new QGridLayout;
+
+    mainLayout->setSizeConstraint(QLayout::SetFixedSize);
+    mainLayout->addWidget(display, 0, 0, 1, 6);
+    mainLayout->addWidget(backspaceButton, 1, 0, 1, 2);
+    mainLayout->addWidget(clearButton, 1, 2, 1, 2);
+    mainLayout->addWidget(clearAllButton, 1, 4, 1, 2);
+    mainLayout->addWidget(clearMemoryButton, 2, 0);
+    mainLayout->addWidget(readMemoryButton, 3, 0);
+    mainLayout->addWidget(setMemoryButton, 4, 0);
+    mainLayout->addWidget(addToMemoryButton, 5, 0);
+    for(int i = 1; i < NumDigitButtons; ++i) {
+        int row = ((9 - i) / 3) + 2;
+        int column = ((i - 1) % 3) + 1;
+        mainLayout->addWidget(digitButtons[i], row, column);
+    }
 }
